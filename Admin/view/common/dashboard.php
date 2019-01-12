a8f12af49ca64e9a05942bdf06ece50f5d9771d8
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-12 21:03:48 +0530

    Update dashboard.php

--- Admin/view/common/dashboard.php
+++ Admin/view/common/dashboard.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
        
-        <link rel="stylesheet" href="http://127.0.0.1/OpenReact/Admin/css/custom.css" />
+        <link rel="stylesheet" href="/Admin/css/custom.css" />
         <script type="text/javascript" src="http://127.0.0.1/OpenReact/Admin/js/react.js"></script>
         <script type="text/javascript" src="http://127.0.0.1/OpenReact/Admin/js/react-dom.js"></script>
          <script type="text/javascript" src="http://127.0.0.1/OpenReact/Admin/js/jquery.js"></script>
