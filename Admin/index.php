8241eeda239e2d7bf8347f4751caa470122535af
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-12 15:25:54 +0530

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -2,15 +2,15 @@
 
 require_once('config.php');
 
-if(is_file(DIR_WIN.'OpenReact\\StartUp.php'))
+if(is_file('../OpenReact/StartUp.php'))
 {
     $service = 'admin';
-    require_once(DIR_WIN.'OpenReact\\StartUp.php');
+    require_once('../OpenReact/StartUp.php');
     Start($service);
 }
 else
 {
-    echo DIR_WIN.'OpenReact\\StartUp.php';
+    echo 'OpenReact/StartUp.php';
     echo "<code>Hard Error Going Down</code>";
     echo "\n<b>Something is wrong with the installation or the config file you can manually fix it </b>";
 }
