01eaf643a995f3adbd10e88057a070556a7187ba
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-12 22:03:09 +0530

    Update admin.php

--- Config/admin.php
+++ Config/admin.php
@@ -8,4 +8,8 @@ $_['default'] = 'common/dashboard';
 $_['helloWorld'] = 'common/dashboard/helloWorld';
 $_['response_compression'] = 0;
 $_['default_cache'] = DIR_APPLICATION.'/Cache/';       
+$_['db_host'] = 'ec2-23-21-171-25.compute-1.amazonaws.com';
+$_['db_name'] = 'd6vfhnp62ap8i6';
+$_['db_user'] = 'krqvewwnavjgor';
+$_['db_password'] = '059ef1053a47b76add380eac93ce02c8de61345669a7598a9fad5b46a39819a5';
 
