bc8f8617d9903830236005c98d575af22be3ee91
rayyanmirza123 <rayyanmirza419@gmail.com>
2019-01-12 14:58:19 +0530

    Update config.php

--- Admin/config.php
+++ Admin/config.php
@@ -7,8 +7,8 @@
 define('DIR_APPNAME','/OpenReact');
 define('DIR_APPLICATION',__DIR__);
 // testing purposes it will remove
-define('DIR_WIN',"C:\\Bitnami\\dreamfactory\\apps\\dreamfactory\\htdocs\\public\\OpenReact\\");
-define('DIR_WIN_CONFIG',DIR_WIN."Config\\");
+//define('DIR_WIN',"C:\\Bitnami\\dreamfactory\\apps\\dreamfactory\\htdocs\\public\\OpenReact\\");
+//define('DIR_WIN_CONFIG',DIR_WIN."Config\\");
 define('DIR_REACT',DIR_APPNAME."/OpenReact/");
 define('DIR_ENGINE',DIR_REACT."Engine/");
 define('DIR_CONFIG',DIR_REACT."Config/");
